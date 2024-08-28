e828c03620d8ddf842d749f9d9ce6e7bc259a8e7
Pit Nahrstedt <p.nahrstedt@dotsource.de>
2024-08-28 16:43:32 +0200

    feat(events): setup service

---
+++ piwik/services/analytics/events.py
@@ -0,0 +1,49 @@
+import datetime
+
+from typing import Any, Optional, Sequence
+
+
+from piwik.base.service import BaseService
+from piwik.schemas.raw import RawAnalyticsParameter
+from piwik.schemas.types import Column, RELATIVE_DATE, FORMAT, COLUMN_FORMAT
+from piwik.utils.validators import validate_response
+
+
+class EventsService(BaseService):
+    _endpoint: str = "/api/analytics/v1/events"
+
+    def get(
+        self,
+        id: str,
+        columns: Sequence[str | Column | dict],
+        page: int = 0,
+        size: int = 100,
+        date_from: Optional[str | datetime.date] = None,
+        date_to: Optional[str | datetime.date] = None,
+        relative_date: Optional[RELATIVE_DATE] = None,
+        format: Optional[FORMAT] = None,
+        column_format: Optional[COLUMN_FORMAT] = None,
+        compression: bool = False,
+    ) -> dict[str, Any] | None:
+        query = RawAnalyticsParameter(
+            website_id=id,
+            columns=columns,
+            page=page,
+            size=size,
+            date_from=date_from,
+            date_to=date_to,
+            relative_date=relative_date,
+            format=format,
+            column_format=column_format,
+        )
+
+        headers = {"Accept-Encoding": "gzip"} if compression else {}
+
+        response = self._client._post(
+            f"{self._endpoint}/",
+            json=query.as_body(),
+            headers=headers,
+        )
+
+        validate_response(response)
+        return response.json()
